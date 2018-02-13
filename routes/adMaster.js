415c779f8472d2df00347321f0188d620e01bf37
alw-AY's <ysc1217@gmail.com>
2018-02-13 12:15:56 +0800

    try

--- routes/adMaster.js
+++ routes/adMaster.js
@@ -2,14 +2,14 @@
  * @Author: alw-AY's
  * @Date:   2018-02-08 16:27:09
  * @Last Modified by:   alw-AY's
- * @Last Modified time: 2018-02-13 12:04:00
+ * @Last Modified time: 2018-02-13 12:06:24
  */
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
 var URL = require('url');
 
-var AdMasterAPI = require('/data/www/default/beacon/getAdMasterData/AdmasterAPI');
+var AdMasterAPI = require('../AdmasterAPI');
 
 var api;
 
